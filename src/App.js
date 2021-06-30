381efa5eb33870563ce5d0eb6f96863c2a77cd62
shikhagupta123 <52396979+shikhagupta123@users.noreply.github.com>
2021-06-30 22:57:21 +0530

    component-practice component practice and stying in jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Heading from "./component/Heading";
 import List from "./component/List";
-
+import Greeting from "./component/Greeting";
 export default function App() {
   return (
     <div>
       <Heading />
       <List />
+      <Greeting />
     </div>
   );
 }
---
+++ src/component/Greeting.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Greeting() {
+  var date = new Date();
+  var gt = date.getHours();
+  var k = "";
+  const cstyl = {
+    color: ""
+  };
+  if (gt < 12) {
+    k = "Good Mprning";
+    cstyl.color = "red";
+  } else if (gt <= 18) {
+    k = "Good Afternoon";
+    cstyl.color = "green";
+  } else {
+    k = "Good Night";
+    cstyl.color = "blue";
+  }
+  return <h1 className="greeting" style={cstyl}>{k}</h1>;
+}
+
+export default Greeting;
